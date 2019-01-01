kerneltoast/android_kernel_oneplus_msm8996

qcom-cpufreq: Clean up underclock code
This is functionally the same as before, but looks much cleaner.

Signed-off-by: Sultanxda <sultanxda@gmail.com>

drivers/cpufreq/qcom-cpufreq.c
@@ -397,21 +397,6 @@ static struct cpufreq_frequency_table *cpufreq_parse_dt(struct device *dev,
 			break;
 		f /= 1000;
 
-#ifdef CONFIG_MACH_MSM8996_15801
-		if (i > 0) {
-			/* Always underclock power cluster for stability */
-			if (cpu < 2) {
-				if (ftbl[i - 1].frequency ==
-						underclk_max_pwrcl)
-					break;
-			} else if (!no_cpu_underclock) {
-				if (ftbl[i - 1].frequency ==
-						underclk_max_perfcl)
-					break;
-			}
-		}
-#endif
-
 		/*
 		 * Check if this is the last feasible frequency in the table.
 		 *
@@ -432,6 +417,21 @@ static struct cpufreq_frequency_table *cpufreq_parse_dt(struct device *dev,
 
 		ftbl[i].driver_data = i;
 		ftbl[i].frequency = f;
+
+#ifdef CONFIG_MACH_MSM8996_15801
+		/* Always underclock power cluster for stability */
+		if (cpu < 2) {
+			if (f == underclk_max_pwrcl) {
+				i++;
+				break;
+			}
+		} else if (!no_cpu_underclock) {
+			if (f == underclk_max_perfcl) {
+				i++;
+				break;
+			}
+		}
+#endif
 	}
 
 	ftbl[i].driver_data = i;

theimprobableoneATSH/android_kernel_zuk_msm8996

Change-Id: Iaa28cf49b23d8b68aaac80b3a828ff7bd9d50dd9

drivers/cpufreq/qcom-cpufreq.c
@@ -457,21 +457,6 @@ static struct cpufreq_frequency_table *cpufreq_parse_dt(struct device *dev,
 			break;
 		f /= 1000;
 
-#ifdef CONFIG_MACH_ZUK_MSM8996
-		if (i > 0) {
-			/* Always underclock power cluster for stability */
-			if (cpu < 2) {
-				if (ftbl[i - 1].frequency ==
-						underclk_max_pwrcl)
-					break;
-			} else if (!no_cpu_underclock) {
-				if (ftbl[i - 1].frequency ==
-						underclk_max_perfcl)
-					break;
-			}
-		}
-#endif
-
 		/*
 		 * Check if this is the last feasible frequency in the table.
 		 *
@@ -492,6 +477,21 @@ static struct cpufreq_frequency_table *cpufreq_parse_dt(struct device *dev,
 
 		ftbl[i].driver_data = i;
 		ftbl[i].frequency = f;
+
+#ifdef CONFIG_MACH_ZUK_MSM8996
+		/* Always underclock power cluster for stability */
+		if (cpu < 2) {
+			if (f == underclk_max_pwrcl) {
+				i++;
+				break;
+			}
+		} else if (!no_cpu_underclock) {
+			if (f == underclk_max_perfcl) {
+				i++;
+				break;
+			}
+		}
+#endif
 	}
 
 	ftbl[i].driver_data = i;